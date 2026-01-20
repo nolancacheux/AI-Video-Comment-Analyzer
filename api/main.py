8d9defe25435e77f7278286c418061ada9587f6d
nolancacheux <cachnolan@gmail.com>
2026-01-20 19:15:39 +0100

    Add port 3001 to CORS allowed origins

--- api/main.py
+++ api/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 # Configure CORS for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
